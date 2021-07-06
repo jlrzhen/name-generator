72e62c229f18c8a98e61c4970cc7793373a43aea
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-05 23:25:35 -0400

    Create buttons and DataMuse api requests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState(null);
+
+  const handleClick = () => {
+    setName("Name");
+  }
+
+  const xhr = new XMLHttpRequest();
+  const url = "https://api.datamuse.com/words?";
+  const params = "rel_jja=name";
+  xhr.responseType = 'json';
+  xhr.open('GET', url + params);
+  xhr.onload = () => {
+    console.log(xhr.response);
+  };
+  xhr.send();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Name Generator</h1>
+        {!name ? <button onClick={handleClick}>Generate Name</button>
+        : <>
+        <h2>Result: {name}</h2>
+        <button onClick={() => {setName(null);}}>New Name</button>
+        </>}            
       </header>
     </div>
   );
